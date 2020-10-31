bf84225f540a915a1589ac85a80e88f269c0fb17
Gianni Narracci <gnarracci@gmail.com>
2020-10-31 02:38:51 -0400

    http file request config

--- api/src/controllers/videos.controllers.ts
+++ api/src/controllers/videos.controllers.ts
@@ -4,7 +4,10 @@ export const getVideos: RequestHandler = (req, res) => {
   res.json("getting videos");
 };
 
-export const createVideo: RequestHandler = (req, res) => {};
+export const createVideo: RequestHandler = (req, res) => {
+  console.log(req.body);
+  res.json("video saved");
+};
 
 export const getVideo: RequestHandler = (req, res) => {};
 
